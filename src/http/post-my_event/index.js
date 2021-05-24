ee7a30045cd86e1db0ad59356cd9d5ef2e87ba19
Kam Lasater <ckl@seekayel.com>
2021-05-24 16:48:17 -0400

    Added time logging on http event

--- src/http/post-my_event/index.js
+++ src/http/post-my_event/index.js
@@ -3,11 +3,12 @@ const arc = require('@architect/functions')
 exports.handler = async function http (req) {
   const name = 'my-event'
   const payload = arc.http.helpers.bodyParser(req)
-  await arc.events.publish({ name, payload })
+  await arc.events.publish({ name, payload, "now": Date.now() })
+  console.log('payload: '+payload)
   return {
     statusCode: 302,
     headers: {
-      location: '/'
+      location: '/?at='+Date.now()
     }
   }
 }
